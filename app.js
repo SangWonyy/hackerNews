d03a4b5a96eefb1d36dd6045e6733d7c80dac76c
sangwonyu <tkddnjs231@naver.com>
2021-06-16 05:47:52 +0900

    dom api 줄이기
    router 기능

--- app.js
+++ app.js
@@ -11,37 +11,50 @@ function getData(url) {
     return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_RUL);
-const ul = document.createElement('ul');
+function newsFeed() {
+    const newsFeed = getData(NEWS_RUL);
+    const newsList = [];
 
-window.addEventListener('hashchange', () => {
-    const id = location.hash.substr(1);
+    newsList.push('<ul>');
+    for(let i = 0; i < 10; i++) {
 
-    const newsContent = getData(CONTENT_URL.replace('@id', id));
-    const title = document.createElement('h1');
-
-    title.innerHTML = newsContent.title;
-
-    content.appendChild(title);
-
-});
-
-for(let i = 0; i < 10; i++) {
-    const div = document.createElement('div');
-
-    div.innerHTML = `
+        newsList.push(`
         <li>
             <a href="#${newsFeed[i].id}">
                 ${newsFeed[i].title} (${newsFeed[i].comments_count})
             </a>
         </li>
-    `;
+    `);
+    }
+    newsList.push('</ul>');
 
-    // ul.appendChild(div.children[0]);
-    ul.appendChild(div.firstElementChild);
+    container.innerHTML = newsList.join('');
+}
+
+function newsDetail() {
+    const id = location.hash.substr(1);
+    const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+    container.innerHTML = `
+        <h1>${newsContent.title}</h1>
+        
+        <div>
+            <a href="#">목록으로</a>
+        </div>
+    `;
+}
+
+function router() {
+    const routePath = location.hash;
+
+    if (routePath === '') {
+        newsFeed();
+    } else {
+        newsDetail();
+    }
 
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+window.addEventListener('hashchange', router);
 
+router();
